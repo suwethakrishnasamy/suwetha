1de9eac16090a8aab9bcc821a393bc039fd0435b
suwethakrishnasamy <suwethak30@gmail.com>
2025-06-14 19:18:48 +0530

    created

---
+++ javascript/script.js
@@ -0,0 +1,70 @@
+var a= "10";
+var b=10;
+// console.log(a+b)
+// console.log(typeof a);
+// var c=null;
+//  console.log(typeof c);
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+var num=2;
+var result=num%2===0? "even":"odd"
+console.log(result);
+
+var mark=60;
+var result=(mark>=90)?"o":(mark>=70)?"a+":(mark>=30)?"pass":"fail"
+console.log(result);
+
+if(mark>=80 && mark<=100){
+    console.log("o");
+}
+else if(mark>=60 && mark<80){
+    console.log("a+");}
+else if(mark>=30 && mark<60){
+    console.log("p");}
+else{
+console.log(f);}
+
+ for(i=1;i<=10;i++){
+    console.log(i);
+    
+ }
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(10,20)
+
+var sum=()=>{
+    var a=10;
+    var b=20;
+    console.log(a+b);
+    
+}
+sum();
+    
+let arr=[10,20,30];
+let arr2=[...arr,70,80,90]; //spread operator to join the previous array with this
+console.log(arr);
+console.log(arr2); 
+
+//destructuring operator
+var marks=[30,40,50,60,80];
+var[m1,m2,m3,m4,m5]=marks
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
+console.log(m5);
+
+
+
+
+    
+
+
+ 
+ 
+
